71f71c9ba3d4d6d5aefc8e353e62cb1b2dcc717f
Lucas Moura <lucasmouraadev@gmail.com>
2022-09-13 02:35:08 -0300

    card initial

--- recipes/views.py
+++ recipes/views.py
@@ -5,14 +5,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
+    return render(request, 'recipes/pages/home.html')
