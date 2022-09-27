9cba55ec6ec40ebfe99e81a4db27620f5627505b
Lucas Moura <lucasmouraadev@gmail.com>
2022-09-27 03:03:13 -0300

    admin config and models config

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,14 @@
-from audioop import maxpp
-
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
 
 
 class Recipe(models.Model):
@@ -20,5 +26,10 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    category = 
-    author =
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+   
