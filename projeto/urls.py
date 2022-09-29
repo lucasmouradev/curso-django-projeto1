81efc23057cb666f477fc8c2ee87bd1b351df1e4
Lucas Moura <lucasmouraadev@gmail.com>
2022-09-29 03:02:33 -0300

    config em settings.py and fix bugs

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 
@@ -23,3 +25,8 @@ urlpatterns = [
     path('', include('recipes.urls')),
     
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 
@@ -33,3 +33,7 @@ class Recipe(models.Model):
         User, on_delete=models.SET_NULL, null=True
     )
    
+
+
+    def __str__(self):
+        return self.title
